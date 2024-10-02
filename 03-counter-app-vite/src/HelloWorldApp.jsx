e6d6e3d0707b1271b51620e3ec34f30e9a095d19
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2024-10-02 08:11:21 +0200

    20241002_0811 Seguimos Avanzando

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+// Es muy importante Tener en Cuenta que los componentes de React hay que designarlos usando Pascal <OpenCamelCase>
+// Siempre la primera Letra de los componentes debe ser en mayúscula
+
+export function App() {
+    // Document.createElement
+    return <h1>Hola Mundo - Ahammm</h1>
+}
+
+ const App_two = () => {
+    // Document.createElement
+    return <h2>Hello World App!!</h2>
+}
+
+export default App_two;
+
+
+
+
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App_two, {App} from './HelloWorldApp'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Create a React component
+
+// Los Componenetes de React son funciones de </JavaScript>
+// Es por esto que a React se le llama Funcional Component. Porque los COMPONENTES SON FUNCIONES
+
+// Es muy importante Tener en Cuenta que los componentes de React hay que designarlos usando Pascal <OpenCamelCase>
+// Siempre la primera Letra de los componentes debe ser en mayúscula
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />  {/* Aquí se renderiza el componente */}
+        <App_two/>
+    </React.StrictMode>
+
+);
+
+// Vite tiene la habilidad de cambiar módulos en caliente.App
+// Esto no es propio de REact, es una funcionalidad de VITE
+// Lo que hace que el funcionamiento sea muy rápido, creo que en Development
