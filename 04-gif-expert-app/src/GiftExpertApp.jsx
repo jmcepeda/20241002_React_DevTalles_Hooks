41b748dee496677bb948b3e0cfe8ebc0fb12c10a
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2024-10-28 07:49:14 +0100

    20241028_0749_ Seguimos Avanzando

--- 04-gif-expert-app/src/GiftExpertApp.jsx
+++ 04-gif-expert-app/src/GiftExpertApp.jsx
@@ -1,7 +1,29 @@
 
 // const apiKey = "jJjbFssbVLY0evvOTsSibXNMI1VMrtm4";
 
+import { useState } from "react"
+
 export const GiftExpertApp = () => {
+
+  // Para mantener el Estado en React hay varios Hook.Estado
+  // Hasta Ahora solo hemos visto un Hook de Estado. UseState
+  // Podemos tener tantos useState como necesitemos
+  // Suele recomendarse definir tantos useState como variables globales estemos utilizando,
+  // pero es posible hacerlo de forma diferente
+  const [categories, setCategories] = useState(["OnePunch", "Dragon Ball"])
+
+  const onaddCategory = ()=> {
+    // console.log("Naruto")
+
+      // En las funciones SetCategories, lo que se trata no es modificar el estado actual sino definir un neuvo estado.
+      // Es por esto que se recomiend no usar .push
+
+    setCategories([...categories, "Naruto"]); // Añado una nueva categoría al estado de categories
+    
+  }
+
+  console.log(categories)
+
   return (
     <>
       {/* Título */}
@@ -11,8 +33,22 @@ export const GiftExpertApp = () => {
 
 
       {/* Listado de Gif  */}
+      <button onClick={onaddCategory}>Agregar</button>
+      <ol>
 
-        {/* Gif Items */}
+        {categories.map((category, index) => {
+
+          return <li key={index}>
+                  {category}
+                  </li>}
+          )
+        }
+
+      </ol>
+
+      {/* Gif Items */}
+
+
     </>
   )
 }
